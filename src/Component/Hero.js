9af0479f0f9a308633cd80f9757d749a5a22a814
Madami <mimadami01@gmail.com>
2024-02-21 16:48:47 +0100

    chanhge

--- src/Component/Hero.js
+++ src/Component/Hero.js
@@ -17,7 +17,7 @@ const Hero = () => {
      
       <div className='py-20 px-10 grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-0 mx-auto  text-black'>
       <div className='mx-auto flex flex-col items-center '>
-          <div className="bg-[white] p-6 py-24 w-120 h-80  text-center   ">
+          <div className="bg-[white] p-6 py-20  text-center   ">
         <h2 className='text-xl md:text-xl text-black font-bold'>All Your Home needs in One Place</h2>
         <p className='text-xl pb-4 md:text-2xl text-[#048392] font-bold'>Green and Convenient</p>
         <p className='text-sm sm:text-sm text-black'>Eco-friendly solutions at Your fingertips  </p>
