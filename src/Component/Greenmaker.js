276e47a52dc44c244aa11d12d73a866a9aafdf2f
Madami <mimadami01@gmail.com>
2024-02-26 00:08:44 +0100

    changes

--- src/Component/Greenmaker.js
+++ src/Component/Greenmaker.js
@@ -10,12 +10,13 @@ function Greenmaker() {
         <div>
           <img className=" md:w-[400px] my-4" src={car} alt="/" />
         </div>
-        <div>
-          <p className='py-4 md:py-20 px-4 md:px-10 mb-4'>
-            Start with Hubbly's eco-friendly approach - from electric vehicles to optimized routes,
-            we're committed to reducing our environmental impact.
-          </p>
-        </div>
+        <div className="text-justify">
+  <p className="py-4 md:py-20 px-4 md:px-10 mb-4">
+    Start with Hubbly's eco-friendly approach - from electric vehicles to optimized routes,
+    we're committed to reducing our environmental impact.
+  </p>
+</div>
+
       </div>
     </div>
   );
