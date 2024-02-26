9acf9de8ad5db8a1f0e37135fc15a3fa9866236a
Madami <mimadami01@gmail.com>
2024-02-26 14:02:47 +0100

    changes

--- src/Component/Greenmaker.js
+++ src/Component/Greenmaker.js
@@ -3,10 +3,10 @@ import car from '../Assets/car.svg';
 
 function Greenmaker() {
   return (
-    <div className='w-full relative font-MuseoModerno py-4 lg:px-40 px-10 text-center'>
+    <div className='w-full relative font-MuseoModerno md:px-40 py-4 lg:px-40 px-10 text-center'>
       <p className='text-[#048392] text-sm md:py-4  '>SUSTAINABLE HOME SERVICES</p>
       <h1 className='text-black font-bold text-3xl px-10 py-2 md:py-2'>What Makes Us Green</h1>
-      <div className='px-4 md:px-10 gap-6  lg:px-20   lg:gap-0 text-black w-full  justify-between items-center  md:flex-row flex-col grid grid-cols-1 md:grid-cols-2'>
+      <div className='px-4  gap-6  lg:px-20   lg:gap-0 text-black w-full  justify-between items-center  md:flex-row flex-col grid grid-cols-1 md:grid-cols-2'>
         <div>
           <img className=" md:w-[400px] my-4" src={car} alt="/" />
         </div>
--- src/Component/Why.js
+++ src/Component/Why.js
@@ -6,7 +6,7 @@ function Why() {
     <div className='w-full relative font-MuseoModerno text-center bg-[#e0f7f9]'>
        <p className='text-[#048392] text-sm md:py-8 py-4 '>QUALITY HOME SERVICES AT THE BEST PRICE</p>
           <h1 className='text-black font-bold text-3xl md:py-2'>Why Choose Hubbly </h1>
-      <div className='px-10 md:px-10 lg:px-40 py-4 md:py-4 grid grid-cols-1 md:grid-cols-2 gap-8 lg:gap-0 text-black mx-auto flex-col '>
+      <div className='px-10 md:px-40 lg:px-60 py-4 md:py-4 grid grid-cols-1 md:grid-cols-2 gap-8 lg:gap-0 text-black mx-auto flex-col '>
         <div className='text-justify'>
           <p className=' px-4 md:px-10 '>Enjoy Quality Home Services at Affordable Prices</p>
           <ul className="text-justify  py-4   md:py-4 px-4  mb-4 md:px-10 text-black text-md">
