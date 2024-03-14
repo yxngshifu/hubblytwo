f8de2d4e704f545c2c8ee53e414f067c2a973440
Madami <mimadami01@gmail.com>
2024-03-14 21:45:12 +0100

    ye

--- src/Component/Privacy.js
+++ src/Component/Privacy.js
@@ -11,82 +11,157 @@ function Privacy() {
           <p className='text-[#048392] text-sm text-center'>PRIVACY POLICY</p>
           <h1 className='text-black font-bold text-4xl text-center'>Hubbly Privacy Policy</h1>
           <p className='py-2 px-10 text-justify'>
-            <h1 className='py-4'>Last Updated: 29th February 2024</h1>
+            <h1 className='py-4'>Last Updated: 9th March 2024</h1>
             Welcome to Hubbly! This Privacy Policy describes how Hubbly ("we," "our," or "us") collects, uses, and shares your information when you use our mobile application, website, and related services (collectively, the "Service"). By accessing or using the Service, you agree to the terms of this Privacy Policy.
           </p>
 
-          <h1 className='text-[#048392] px-10'>1. Information We Collect</h1>
+           <h1 className='text-[#048392] px-10'>1.Definitions</h1>
           <p className='py-2 px-10 text-justify'>
-            <h1 className=' py-2'>1.1 Personal Information</h1>
+            In these Privacy Policy, the following definitions will apply:
+            <ul className='py-2 px-6 text-justify list-disc'>
+              <li className=' py-2'>
+               "Account": refers to a User's personalised and dedicated digital space on the Platform.
+              </li>
+              <li className=' py-2'>
+               "Application": denotes the mobile application operated by Hubbly for Hubblees only, available on iOS and Android.
+               </li>
+               <li className=' py-2'>
+               "Booking": signifies a reservation of a Domestic Service made by a Hubblee.
+               </li>
+               <li className=' py-2'>
+               "Content": encompasses all comments, messages, information, and data uploaded to the Platform.
+               </li>
+               <li className=' py-2'>
+               "Domestic Service": pertains to the provision of services at home by a Hubbler to a Hubblee, including cleaning, hair care, dog walking, and other home-based services offered on the Platform.
+               </li>
+                <li className=' py-2'>
+               "Hubbler": denotes a service provider operating as a company or self-employed, duly registered on the Platform, possessing the necessary skills to provide Domestic Services.
+               </li>
+                <li className=' py-2'>
+               "Hubbly": refers to HUBBLY.ME LTD, registered with Companies House, Company Number: 15347475.
+               </li>
+                <li className=' py-2'>
+               “Regulation”: Signifies the UK Data Protection Act 2018 and any other relevant legislation governing data protection in the United Kingdom.
+               </li>
+                <li className=' py-2'>
+               Additionally, terms under the UK Data Protection Act 2018 are interpreted in accordance with the provisions of the Act. For further clarification on personal data-specific vocabulary, we recommend consulting the guidance provided by the Information Commissioner's Office (ICO), the UK's independent authority on data protection.
+               </li>
+                <li className=' py-2'>
+               "Hubbly Application": represents the mobile application operated by Hubbly, available on iOS and Android.
+               </li>
+               <li className=' py-2'>
+               "Hubbly Services": encompasses all services Hubbly provides to Users, such as facilitating connections between Hubblers and Hubblees, invoice and payment tools, etc.
+               </li>
+           <li className=' py-2'>
+               "Hubblee": signifies a consumer using the Platform with the aim of being connected with a Hubbler to receive a Domestic Service.
+               </li>
+                <li className=' py-2'>
+               "Platform": denotes the contact platform operated by Hubbly, accessible via the Website, the Application, and the Hubbly Application, enabling the provision of Hubbly Services.
+               </li>
+                <li className=' py-2'>
+               "T&Cs": refers to these terms and conditions.
+               </li>
+                <li className=' py-2'>
+               "User": indicates either a Hubblee or a Hubbler who utilizes the Platform.
+               </li>
+               <li className=' py-2'>
+               "Website": represents the website located at https://www.hubbly.me/ as well as all sub-domains.
+               </li>
+              </ul>
+              These definitions establish clarity and understanding for all parties involved in using the Platform and accessing Hubbly Services.
+          </p>
+
+          <h1 className='text-[#048392] px-10'>2. Information We Collect</h1>
+          <p className='py-2 px-10 text-justify'>
+            <h1 className=' py-2'>2.1 Personal Information</h1>
             When Hubblee and Hubbler create a Hubbly account, we may collect personal information such as their name, email address, phone number, and payment details.
-            <h1 className=' py-2'>1.2 Additional Information</h1>
+            <h1 className=' py-2'>2.2 Additional Information</h1>
             We may collect additional information from Hubblee and Hubbler, including their location, service preferences, and feedback.
-            <h1 className=' py-2'>1.3 Information from Third Parties:</h1>
+            <h1 className=' py-2'>2.3 Information from Third Parties:</h1>
             We may receive information about Hubblee and Hubbler from third-party services, such as social media platforms, if they choose to link their account.
           </p>
 
-          <h1 className='text-[#048392] px-10'>2. How We Use Your Information</h1>
+          <h1 className='text-[#048392] px-10'>3. How We Use Your Information</h1>
           <p className='py-2 px-10 text-justify'>
-            <h1 className=' py-2'>2.1 Provide and Improve Services:</h1>
+            <h1 className=' py-2'>3.1 Provide and Improve Services:</h1>
             We use Hubblee' and Hubbler' information to provide and improve our services, including matching them with service requests, processing payments, and enhancing user experience.
-            <h1 className=' py-2'>2.2 Communications:</h1>
+            <h1 className=' py-2'>3.2 Communications:</h1>
             We may use Hubblee' and Hubbler' information to communicate with them about their account, service requests, promotions, surveys, and updates.
-            <h1 className=' py-2'>2.3 Analytics and Research:</h1>
+            <h1 className=' py-2'>3.3 Analytics and Research:</h1>
             We may use Hubblee' and Hubbler' information for data analysis, research, and to improve the effectiveness of our services and marketing efforts.
-            <h1 className=' py-2'>2.4 Improvements and Modifications:</h1>
+            <h1 className=' py-2'>3.4 Improvements and Modifications:</h1>
             Hubbly continuously develops and enhances its services. We may use collected data to improve the quality and operation of the Hubbly platform.
-            <h1 className=' py-2'>2.5 Personalization:</h1>
+            <h1 className=' py-2'>3.5 Personalization:</h1>
             We may use your information to personalize your experience and provide content and services tailored to your preferences.
           </p>
- <h1 className='text-[#048392] px-10'>3. Your Choices</h1>
+ <h1 className='text-[#048392] px-10'>4. Your Choices</h1>
  <p className='py-2 px-10  text-justify'>
-<h1 className=' py-2'>3.1 Account Information:</h1>
+<h1 className=' py-2'>4.1 Account Information:</h1>
 Hubblee and Hubbler can review and update their account information at any time by logging into their Hubbly account.
-<h1 className=' py-2'>3.2 Communication Preferences:</h1>
+<h1 className=' py-2'>4.2 Communication Preferences:</h1>
 Hubblee and Hubbler can choose to opt-out of promotional communications by following the unsubscribe instructions included in the emails we send or by contacting us directly.
-<h1 className=' py-2'>3.3 Location Information:</h1>
+<h1 className=' py-2'>4.3 Location Information:</h1>
 Hubblee and Hubbler can disable location tracking through their device settings, but this may affect certain features of the Service.
-<h1 className=' py-2'>3.4 Modification of Information:</h1>
+<h1 className=' py-2'>4.4 Modification of Information:</h1>
 Users can modify their personal information through the Hubbly app.
-<h1 className=' py-2'>3.5 Data Access and Deletion:</h1>
+<h1 className=' py-2'>4.5 Data Access and Deletion:</h1>
 Users have the right to access their personal information stored by Hubbly and request deletion of their data. Requests for data access or deletion can be made by contacting us at [info@hubbly.me].
 </p>
- <h1 className='text-[#048392] px-10'>4. Children's Privacy</h1>
+ <h1 className='text-[#048392] px-10'>5. Children's Privacy</h1>
  <p className='py-2 px-10  text-justify'>
-    <h1 className=' py-2'>4.1 Age Limitation:</h1>
+    <h1 className=' py-2'>5.1 Age Limitation:</h1>
 The Service is not intended for children under the age of 18. We do not knowingly collect personal information from children under 18.
 Hubbly does not knowingly collect personal information from children under the age of 13. If you are a parent or guardian and believe that your child has provided us with personal information, please contact us immediately at [contact@hubblyapp.com]. If we become aware that we have collected personal information from children without verification of parental consent, we will take steps to remove that information from our servers.
 This Privacy Policy applies solely to information collected by the Hubbly app and does not apply to information collected by any third-party sites or services linked to or from the Hubbly app.
 By using the Hubbly app, you signify your acceptance of this Privacy Policy. If you do not agree to this policy, please do not use our app.
  </p>
- <h1 className='text-[#048392] px-10'>5. Information Sharing</h1>
+ <h1 className='text-[#048392] px-10'>6. Information Sharing</h1>
 <p className='py-2 px-10  text-justify'>
-    <h1 className=' py-2'>5.1 Service Providers:</h1>
+    <h1 className=' py-2'>6.1 Service Providers:</h1>
 We may share Hubblee' and Hubbler' information with third-party service providers who help us operate, provide, and improve our services.
-<h1 className=' py-2'>5.2 Legal Compliance:</h1>
+<h1 className=' py-2'>6.2 Legal Compliance:</h1>
 We may disclose Hubblee' and Hubbler' information if required by law or in response to valid requests by public authorities (e.g., law enforcement).
 </p>
- <h1 className='text-[#048392] px-10'>6. Data Security</h1>
+ <h1 className='text-[#048392] px-10'>7. Data Security</h1>
 <p className='py-2 px-10  text-justify'>
-    <h1 className=' py-2'>6.1 Security Measures:</h1>
+    <h1 className=' py-2'>7.1 Security Measures:</h1>
 We prioritize the security of your information. However, no method of transmission over the internet or electronic storage is 100% secure. While we strive to use commercially acceptable means to protect your personal information, we cannot guarantee its absolute security.
 We take reasonable measures to protect Hubblee' and Hubbler' information from unauthorized access, disclosure, alteration, or destruction.
-<h1 className=' py-2'>6.2 Data Retention:</h1>
+<h1 className=' py-2'>7.2 Data Retention:</h1>
 We retain Hubblee' and Hubbler' information for as long as necessary to fulfill the purposes outlined in this Privacy Policy, unless a longer retention period is required or permitted by law.
 </p>
-<h1 className='text-[#048392] px-10'>7. Changes to this Privacy Policy</h1>
+<h1 className='text-[#048392] px-10'>8. Changes to this Privacy Policy</h1>
 <p className='py-2 px-10  text-justify'>
-    <h1 className=' py-2'>7.1 Notification of Changes</h1>
+    <h1 className=' py-2'>8.1 Notification of Changes</h1>
 We may update this Privacy Policy from time to time. We will notify Hubblee and Hubbler of any material changes by posting the new Privacy Policy on this page.
-<h1 className=' py-2'>7.2 Your Continued Use</h1>
+<h1 className=' py-2'>8.2 Your Continued Use</h1>
 Hubblee' and Hubbler' continued use of the Service after we post any modifications to the Privacy Policy will constitute their acknowledgment of the modifications and their consent to abide and be bound by the modified Privacy Policy.
 </p>
-<h1 className='text-[#048392] px-10'>8. Contact Us</h1>
+<h1 className='text-[#048392] px-10'>9. 9.Your Rights</h1>
 <p className='py-2 px-10  text-justify'>
-    <h1 className=' py-2'>8.1 Questions or Concerns</h1>
+    <h1 className=' py-2'>9.1 Your Rights</h1> 
+    In accordance with the Regulation, users possess various rights concerning their personal data:
+    <ul className='py-2 px-6 text-justify list-disc'>
+              <li className=' py-2'>
+              Right of Access: Users can request access to their personal data and obtain a copy.
+              </li>
+              <li className=' py-2'>
+            Right to Rectification and Erasure: Users can request correction or deletion of inaccurate or obsolete personal data.
+               </li>
+ <li className=' py-2'>Right to Object and Restriction of Processing: Users can object to or request restriction of processing, subject to legitimate grounds.</li>
+<li className=' py-2'>Right to Data Portability: Users can request data in a structured, machine-readable format for transmission to another data controller.</li>
+<li className=' py-2'>Right to Lodge a Complaint: Users can lodge complaints with relevant data protection authorities if Hubbly is believed to be non-compliant with data protection rules.</li>
+</ul>
+<h1 className='text-[#048392] px-10'>9.2 Exercise Your Rights</h1>
+Exercise of these rights is subject to certain conditions and may be restricted or limited. Users must verify their identity for each request and provide necessary details for response communication. Hubbly aims to respond within a reasonable timeframe, typically within one month, reserving the right to charge fees for excessive or unfounded requests.
+</p>
+
+<h1 className='text-[#048392] px-10'>10. Contact Us</h1>
+<p className='py-2 px-10  text-justify'>
+    <h1 className=' py-2'>10.1 Questions or Concerns</h1>
 If you have any questions or concerns about this Privacy Policy or our data practices, please contact us at [admin@hubbly.me].
 Please review the complete Privacy Policy on our website for more detailed information about how we collect, use, and share your information.
-Effective Date: 29th February 2023</p>
+Effective Date: 9th March 2024</p>
           
           </div>
           </div>
