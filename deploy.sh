bb83d05e2b67ccac1e1841815a1e1f28f95313c8
Madami <mimadami01@gmail.com>
2024-03-17 18:12:59 +0100

    ye

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+echo "switching to branch master"
+git checkout master
+
+echo "Building app...."
+npm run build
+
+echo "Deploying files to server....."
+scp -r build/* root@139.162.225.178:/var/www/139.162.225.178/
+
+echo "Done!"
--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -5,11 +5,12 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
+  const [mobileNav, setMobileNav] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
-      const currentScrollY = window.scrollY;
-      if (currentScrollY > 0) {
+      const isTop = window.scrollY < 100;
+      if (!isTop) {
         setNav(true);
       } else {
         setNav(false);
@@ -23,79 +24,80 @@ const Navbar = () => {
     };
   }, []);
 
-  const handleNav = () => {
-    setNav(!nav);
+  const handleMobileNav = () => {
+    setMobileNav(!mobileNav);
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
   };
 
   return (
     <div className={`font-MuseoModerno bg-transparent fixed top-0 left-0 right-0 z-10 ${nav ? 'hidden' : ''}`}>
       <div className="flex justify-between items-center mx-auto px-4 text-black">
         <ul className="space-x-4 text-black font-bold p-4 flex items-center ml-[-10px]">
-          <Link to="/">
+          <Link to="/" onClick={scrollToTop}>
             <img className="w-[100px] mx-4 my-4 flex items-center" src={logo1} alt="/" />
           </Link>
           <li className="hidden md:block hover:text-[#2a9d8f] cursor-pointer">
-            <Link to="/aboutus">
+            <Link to="/aboutus" onClick={scrollToTop}>
               About Us
             </Link>
           </li>
           <li className="hidden md:block hover:text-[#2a9d8f] cursor-pointer">
-            <Link to="/support">
+            <Link to="/support" onClick={scrollToTop}>
               Goals We Support
             </Link>
           </li>
           <li className="hidden md:block hover:text-[#2a9d8f] cursor-pointer">
-            <Link to="/hubblyworks">
+            <Link to="/hubblyworks" onClick={scrollToTop}>
               How Hubbly Works
             </Link>
           </li>
           <li className="hidden md:block hover:text-[#2a9d8f] cursor-pointer">
-            <Link to="/ourofferings">
+            <Link to="/ourofferings" onClick={scrollToTop}>
               Our Offerings
             </Link>
           </li>
         </ul>
 
-        <div onClick={handleNav} className="block md:hidden" style={{ color: 'black' }}>
-          {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+        <div onClick={handleMobileNav} className="block md:hidden" style={{ color: 'black', cursor: 'pointer' }}>
+          {mobileNav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
         </div>
 
-        <div
-          className={
-            nav
-              ? 'fixed left-0 top-16 w-full h-full bg-[#003049] text-white ease-in-out duration-500'
-              : 'hidden'
-          }
-        >
-          <Link to='/'>
-            <h1 className='w-full font-bold text-3xl m-4 text-[#048392] hidden md:block'>HUBBLY</h1>
-          </Link>
+        {mobileNav && (
+          <div className="fixed left-0 top-16 w-full h-full bg-[#003049] text-white ease-in-out duration-500">
+            <Link to='/' onClick={scrollToTop}>
+              <h1 className='w-full font-bold text-3xl m-4 text-[#048392]'>HUBBLY</h1>
+            </Link>
 
-          {nav && (
             <ul className='flex-col w-full space-y-12 text-white font-bold p-4 items-center'>
               <li>
-                <Link to="/aboutus">
+                <Link to="/aboutus" onClick={scrollToTop}>
                   About Us
                 </Link>
               </li>
               <li>
-                <Link to="/support">
+                <Link to="/support" onClick={scrollToTop}>
                   Goals we support
                 </Link> 
               </li>
-              <li className="">
-                <Link to="/hubblyworks">
+              <li>
+                <Link to="/hubblyworks" onClick={scrollToTop}>
                   How Hubbly Works
                 </Link>
               </li>
-              <li className="">
-                <Link to="/ourofferings">
+              <li>
+                <Link to="/ourofferings" onClick={scrollToTop}>
                   Our Offerings
                 </Link>
               </li>
             </ul>
-          )}
-        </div>
+          </div>
+        )}
       </div>
     </div>
   );
